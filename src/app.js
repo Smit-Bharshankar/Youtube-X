f6ca6f29708cceed2cd582847db6a73ee9e22028
Smit <Smit-Bharshankar@users.noreply.github.com>
2024-12-18 23:20:13 +0530

    added utils

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
+
+const app = express()
+
+// app.use is used for middleware or configuration
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}))
+
+app.use(express.json({limit: "16kb"}))    // for accepting json data
+app.use(express.urlencoded({extended: true , limit: "16kb"})) // for accepting encoded data from url
+app.use(express.static("public"))   // for storing assests in public 
+app.use(cookieParser())          // for reading and writing cookies in user's browser
+
+export { app }
--- src/index.js
+++ src/index.js
@@ -6,4 +6,15 @@ import connectDB from "./db/index.js";
 
 dotenv.config({ path: './env'})
 
-connectDB();
+const port = process.env.PORT
+connectDB()
+.then((e) => {
+    app.listen(port || 8000 , () => {
+        console.log(`Server is running at the port ${(port || 8000)}`);
+        
+    })
+})
+.catch( (e) => {
+    console.log('Mongodb connection failed :' , e);
+    
+})
