a293724e3826649cfcb86509f2db8d6cd771ddc7
Smit <Smit-Bharshankar@users.noreply.github.com>
2024-12-21 12:36:58 +0530

    added user and video model with some middlewares and configured jwt access & refresh tokens

---
+++ src/models/video.model.js
@@ -0,0 +1,46 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+
+
+const videoSchema = new mongoose.Schema(
+    {
+        videoFile: {
+            type: String, // url from cloudinary
+            required: true
+        } ,
+        thumbnail: {
+            type: String, // url from cloudinary
+            required: true
+        } ,
+        title: {
+            type: String,
+            required: true
+        } ,
+        description: {
+            type: String,
+            required: true
+        } ,
+        duration: {
+            type: Number,  // get from cloudinary
+            required: true
+        } ,
+        views: {
+            type: Number,
+            default: 0
+        } ,
+        isPublished: {
+            type: Boolean,
+            default: true
+        } ,
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+
+    },{timestamps: true})
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+
+export const Video = mongoose.model("Video" , videoSchema)
