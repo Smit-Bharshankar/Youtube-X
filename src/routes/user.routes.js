5cfa19befb731d23d2abb01ae793a5b1dbc1d4c1
Smit <Smit-Bharshankar@users.noreply.github.com>
2024-12-24 23:03:38 +0530

    added new routes for user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
-import {registerUser , loginUser , logoutUser , refreshAccessToken} from "../controllers/user.controller.js";
+import {registerUser,
+     loginUser,
+     logoutUser,
+     refreshAccessToken,
+     changeCurrentPassword,
+     getCurrentUser,
+     updateAccountDetails,
+     updateAvatar,
+     updateUserCoverImage,
+     getUserChannelProfile,
+      } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js"
 
@@ -23,6 +33,9 @@ router.route("/login").post(loginUser)
 // secured routes
 router.route("/logout").post(verifyJwt , logoutUser)
 router.route("/refreshtoken").post(refreshAccessToken)
+
+router.route("/current-user").get(verifyJwt, getCurrentUser)
+router.route("/update-account").patch(verifyJwt, updateAccountDetails)
 
 
 
